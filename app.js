5927b71a9ca9357b00c6b7fd78e7dc7f9afad22f
telaak <teemu@laaksonen.me>
2018-10-03 19:33:17 +0300

    websocket

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 'use strict'
 
 const express = require('express')
-let cors = require('cors')
-
-const locationsmemory = require('./routes/locations.js')
+const cors = require('cors')
+const locationRouter = require('./routes/locations.js')
 
 const app = express()
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const locations = new locationRouter(http, io)
 app.use(cors())
 
 app.use(function(req, res, next) {
@@ -15,8 +19,12 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.json())
-app.use('/locations', locationsmemory)
+app.use('/locations', locations.router)
 
-const server = app.listen(8080, () => {
-  console.log(`Listening on port ${server.address().port}`)
+app.get('/', function(res, req) {
+  res.send("Index test")
+})
+
+http.listen(8080, () => {
+  console.log(`Listening on port :8080`)
 })
--- routes/locations.js
+++ routes/locations.js
@@ -1,35 +1,40 @@
-'use strict'
-
 const express = require('express')
 const crudRepository = require('../database/crudrepository.js')
 
-let locations = express.Router()
+class Locations {
+  constructor(http, io) {
+    this.http = http
+    this.io = io
+    this.router = express.Router()
+    this.router.get('/', (req, res) => {
+      crudRepository.findAll((result) => res.send(result))
+    })
 
-locations.get('/', (req, res) => {
-  crudRepository.findAll((result) => res.send(result))
-})
+    this.router.get('/:urlId([1-9]+)', (req, res) => {
+      const urlId = Number(req.params.urlId)
+      crudRepository.findById(urlId, (result) => res.send(result),
+        () => res.status(404).send({ "error": "Can't find with given id." }))
+    })
 
-locations.get('/:urlId([1-9]+)', (req, res) => {
-  const urlId = Number(req.params.urlId)
-  crudRepository.findById(urlId, (result) => res.send(result), 
-    () => res.status(404).send({"error": "Can't find with given id."}) )
-})
+    this.router.post('/', (req, res) => {
+      let location = { "lat": req.body.lat, "lon": req.body.lon }
+      crudRepository.save(location, (idNumber) => {
+        let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl + idNumber;
+        res.location(fullUrl)
+        res.status(201)
+        res.end()
+        io.emit('newLoc', fullUrl)
+      })
+    })
 
-locations.post('/', (req, res) => {
-  let location = {"lat": req.body.lat, "lon": req.body.lon}
-  crudRepository.save(location, (idNumber) => {
-    let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl + idNumber;
-    res.location(fullUrl)
-    res.status(201)
-    res.end()
-  })
-})
+    this.router.delete('/:urlId([1-9]+)', (req, res) => {
+      const urlId = Number(req.params.urlId)
+      crudRepository.deleteById(urlId, () => {
+        res.status(204).end()
+        io.emit('locDeleted', urlId)
+      }, () => res.status(404).send({ "error": "Can't find with given id." }))
+    })
+  }
+}
 
-locations.delete('/:urlId([1-9]+)', (req, res) => {
-  const urlId = Number(req.params.urlId)
-  crudRepository.deleteById(urlId, () => {
-      res.status(204).end()
-  }, () => res.status(404).send({"error": "Can't find with given id."}))
-})
-
-module.exports = locations
+module.exports = Locations
